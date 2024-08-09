df7381dd0dfe6d30a31a3ef42a0606331539db20
blincoln15 <bglincol@hawaii.edu>
2024-08-09 12:21:42 -1000

    feat: firstname fieldset
    This feature adds the text and user input box for firstName field to form page.

--- folder-structure/src/App.js
+++ folder-structure/src/App.js
@@ -11,13 +11,70 @@ function App() {
     const [email, setEmail] = useState("");
     const [contact, setContact] = useState("");
     const [gender, setGender] = useState("male");
+    const [subjects, setSubjects] = useState({
+        english: true,
+        maths: false,
+        physics: false
+    });
+    const [resume, setResume] = useState("");
+    const [url, setUrl] = useState();
+    const [selectedOption, setSelectedOption] = useState("");
+    const [about, setAbout] = useState("");
 
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(
+            firstName,
+            lastName,
+            email,
+            contact,
+            gender,
+            selectedOption,
+            subjects,
+            resume,
+            url,
+            about
+        );
+    }
 
+    const handleReset = () => {
+        setFirstName("");
+        setLastName("");
+        setEmail("");
+        setContact("");
+        setGender("female");
+        setSubjects({
+            english: false,
+            maths: false,
+            physics: false,
+        });
+        setResume("");
+        setUrl("");
+        setSelectedOption("");
+        setAbout("");
+    };
 
     return (
         <div className="App">
             <h1>Form in React</h1>
-            
+            <fieldset>
+                <form action="#" method="get">
+                    <label for="firstname">
+                        First Name* 
+                    </label>
+                    <input
+                        type="text"
+                        name="firstname"
+                        id="firstname"
+                        value={firstName}
+                        onChange={(e) =>
+                            setFirstName(e.target.value)
+                        }
+                        placeholder="Enter First Name"
+                        required
+                    />
+                </form>
+            </fieldset>
         </div>
     );
 }
